dda4cc4509c50bd9b0d5eb0360661b58f3cf99bd
Yehor <Yehor@KOMPUTER>
2024-12-11 20:53:38 +0200

    Test on laptop

---
+++ TestingApp/RegistrationForm.cs
@@ -0,0 +1,15 @@
+namespace TestingApp
+{
+    public partial class RegistrationForm : Form
+    {
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
