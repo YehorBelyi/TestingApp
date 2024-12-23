ae479c5aa9b8366e8bec83169138b804c4c1bb1c
Yehor <Yehor@KOMPUTER>
2024-12-23 13:31:24 +0200

    Revert "Test on laptop"
    This reverts commit dda4cc4509c50bd9b0d5eb0360661b58f3cf99bd.

--- TestingApp/Form1.cs
+++ TestingApp/Form1.cs
@@ -1,13 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace TestingApp
 {
     public partial class Form1 : Form
@@ -15,6 +5,11 @@ namespace TestingApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
